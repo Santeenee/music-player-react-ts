29a4d3ed20dd92dff950d4bdbf0e09ae7f328b5e
Francesco Santini <francescotn02@gmail.com>
2023-01-27 09:36:41 +0100

    minor fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import '/dist/output.css'
+// import '/dist/output.css'
 
 const rootElem = document.getElementById('root') as HTMLElement
 ReactDOM.createRoot(rootElem).render(<App />)
